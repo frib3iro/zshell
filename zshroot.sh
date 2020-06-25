a6aca25e720e0b48a44a4af7432bda011eee949e
Fabio Junior Ribeiro <rib3iro@live.com>
2020-06-25 15:21:15 -0300

    first commit

--- zshroot.sh
+++ zshroot.sh
@@ -6,10 +6,25 @@ az='\e[34;1m'
 fim='\e[m'
 
 # Importar as configuracoes e temas do OhMyZsh para o usuario root
-sudo cp /home/$USER/.zshrc /root
-sudo cp -r /home/$USER/.vim /root
-sudo cp -r /home/$USER/.oh-my-zsh /root
-sudo cp /home/$USER/.vimrc /root
-sudo sed -i 's#export ZSH="/$USER/.oh-my- zsh"#export ZSH="/root/.oh-my- zsh"#g' /root/.zshrc
+echo -e "${az}Copiando .zshrc para /root${fim}"
+sleep 2s
+[ sudo cp /home/$USER/.zshrc /root ] && echo "Sucesso" || echo "Falhou!"
+
+echo -e "${az}Copiando .vim para /root${fim}"
+sleep 2s
+[ sudo cp -r /home/$USER/.vim /root ] && echo "Sucesso" || echo "Falhou!"
+
+echo -e "${az}Copiando .oh-my-zsh para /root${fim}"
+sleep 2s
+[ sudo cp -r /home/$USER/.oh-my-zsh /root ] && echo "Sucesso" || echo "Falhou!"
+
+echo -e "${az}Copiando o vimrc para /root${fim}"
+sleep 2s
+[ sudo cp /home/$USER/.vimrc /root ] && echo "Sucesso" || echo "Falhou!"
+
+echo -e "${az}Editando o arquivo .zshrc${fim}"
+sleep 2s
+[ sudo sed -i 's#export ZSH="/$USER/.oh-my- zsh"#export ZSH="/root/.oh-my-zsh"#g' /root/.zshrc ] && echo "Sucesso" || echo "Falhou!"
+
 echo -e "${az}OhMyZsh to Root ${seta} Operação concluída${fim}"
 
